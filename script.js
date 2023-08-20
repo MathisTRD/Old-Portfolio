e9070c05256779d110af7ac6f60f7140181e535c
Mathis <mattyyytrd@gmail.com>
2023-08-21 01:29:53 +0200

    new me

--- script.js
+++ script.js
@@ -1,28 +1,23 @@
-var slidePosition = 1;
-SlideShow(slidePosition);
+document.addEventListener("DOMContentLoaded", function () {
+  const storyBoxes = document.querySelectorAll('.story-box');
+  const expandedOverlay = document.querySelector('.expanded-overlay');
+  const expandedBox = document.querySelector('.expanded-box');
+  const expandedContent = document.querySelector('.expanded-content');
+  const closeBtn = document.querySelector('.close-btn');
 
-// forward/Back controls
-function plusSlides(n) {
-  SlideShow(slidePosition += n);
-}
+  storyBoxes.forEach((box, index) => {
+    box.addEventListener('click', () => {
+      const content = box.cloneNode(true);
+      expandedContent.innerHTML = content.innerHTML;
+      expandedOverlay.style.display = 'block';
+      expandedBox.style.display = 'block';
+      document.body.style.overflow = 'hidden';
+    });
+  });
 
-//  images controls
-function currentSlide(n) {
-  SlideShow(slidePosition = n);
-}
-
-function SlideShow(n) {
-  var i;
-  var slides = document.getElementsByClassName("Containers");
-  var circles = document.getElementsByClassName("dots");
-  if (n > slides.length) {slidePosition = 1}
-  if (n < 1) {slidePosition = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < circles.length; i++) {
-      circles[i].className = circles[i].className.replace(" enable", "");
-  }
-  slides[slidePosition-1].style.display = "block";
-  circles[slidePosition-1].className += " enable";
-} 
+  closeBtn.addEventListener('click', () => {
+    expandedOverlay.style.display = 'none';
+    expandedBox.style.display = 'none';
+    document.body.style.overflow = 'auto';
+  });
+});
